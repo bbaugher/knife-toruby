80f6428216b5dad2dc7f807771efda979c21d409
Bryan Baugher <bryan.baugher@cerner.com>
2014-09-17 15:13:33 -0500

    Initial commit

---
+++ lib/knife/toruby.rb
@@ -0,0 +1,7 @@
+require "knife/toruby/version"
+
+module Knife
+  module Toruby
+    # Your code goes here...
+  end
+end
---
+++ lib/knife/toruby/version.rb
@@ -0,0 +1,5 @@
+module Knife
+  module Toruby
+    VERSION = "0.0.1"
+  end
+end
